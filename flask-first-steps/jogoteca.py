e39d87584cfb597678e726d3399b9a6dce52201e
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-05-15 17:16:40 -0300

    Melhorando o código e a usuabilidade
    - Como fazer redirecionamento de páginas
    - Como utilizar o bootstrap para estilizar nossa página
    - Como reutilizar trechos do template
    - Como gerar URLs dinâmicas

--- flask-first-steps/jogoteca.py
+++ flask-first-steps/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -29,8 +29,6 @@ def criar():
     jogo = Jogos(nome, categoria, console)
 
     lista.append(jogo)
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
-
-
+    return redirect('/')
 
 app.run(debug=True)
